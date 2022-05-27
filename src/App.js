4abb5a2965274e777842fcb265ed81638ddf207c
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-27 23:47:12 +0300

    [FE] login: cleaned code

--- src/App.js
+++ src/App.js
@@ -14,38 +14,11 @@ export default function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              crypto-exchange-client
+              Crypto Exchange
             </Typography>
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-            >
-              <GitHubIcon />
-            </IconButton>
           </Toolbar>
         </AppBar>
       </Box>
-      <Box sx={{ p: 3 }}>
-        <Typography variant="h1">h1. Heading</Typography>
-        <Typography variant="h2">h2. Heading</Typography>
-        <Typography variant="h3">h3. Heading</Typography>
-        <Typography variant="h4">h4. Heading</Typography>
-        <Typography variant="h5">h5. Heading</Typography>
-        <Typography variant="h6">h6. Heading</Typography>
-        <Typography variant="body1">body1. text</Typography>
-        <Typography variant="body2">body2. text</Typography>
-      </Box>
-      <Fab
-        color="secondary"
-        sx={{
-          position: 'fixed',
-          right: ({ spacing }) => spacing(3),
-          bottom: ({ spacing }) => spacing(3),
-        }}
-      >
-        <AddIcon />
-      </Fab>
     </>
   )
 }
