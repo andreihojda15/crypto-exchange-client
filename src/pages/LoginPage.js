2df0bc52e8d1426431918141be618f6f7157f379
andreihojda15 <andrei.hojda00@gmail.com>
2022-06-05 19:32:11 +0300

    [FE] login: default login implemented

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './loginPage.css'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLoginClick = () => {
+        axios.post('http://localhost:1234/auth/login', {
+            username,
+            password
+        }, { withCredentials: true }).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    const handleGithubLogin = () => {
+        axios.get('http://localhost:1234/auth/github', { withCredentials: true })
+            .then((res) => {
+                console.log(res);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const handleGoogleLogin = () => {
+        axios.get('http://localhost:1234/auth/google')
+            .then((res) => {
+                console.log(res);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const LoginSchema = Yup.object().shape({
+        username: Yup.string()
+            .min(2, 'Too short')
+            .max(50, 'Too long')
+            .required('Required'),
+        password: Yup.string()
+            .min(2, 'Too short')
+            .max(50, 'Too long')
+            .required('Required'),
+    })
+
     return (
         <div>
             <Box sx={{
                 display: 'flex',
                 flexWrap: 'wrap',
                 '& > :not(style)': {
-                    m: 20,
+                    m: 15,
                     width: 480,
                     height: 586,
                 },
@@ -33,19 +80,38 @@ const LoginPage = () => {
                         }
                     }}>
                         <Typography className='loginText' variant='h3'>Login</Typography>
-                        <TextField className='username' size='small' id="outlined-basic" label="Username" variant="outlined" />
-                        <TextField className='password' size='small' id="outlined-basic" type='password' label="Password" variant="outlined" />
-                        <Box>
-                            <Button
-                                className='button'
-                                color="primary"
-                                variant="contained"
-                                type="submit"
-                            >
-                                Login
-                            </Button>
-                        </Box>
-                        <Box sx={{
+                        <Formik
+                            initialValues={{
+                                username: '',
+                                password: ''
+                            }}
+                            validationSchema={LoginSchema}
+                            onSubmit={values => {
+                                // same shape as initial values
+                                console.log(values);
+                            }}
+                        >
+                            {({ errors, touched }) => (
+                                <Form className='form'>
+                                    <TextField className='username' size='small' id="outlined-basic" label="Username" variant="outlined" value={username} onChange={(e) => { setUsername(e.target.value) }} />
+                                    <TextField className='password' size='small' id="outlined-basic" type='password' label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                    <Box>
+                                        <Button
+                                            className='button'
+                                            color="primary"
+                                            variant="contained"
+                                            type="submit"
+                                            fullWidth
+                                            size='large'
+                                            onClick={handleLoginClick}
+                                        >
+                                            Login
+                                        </Button>
+                                    </Box>
+                                </Form>
+                            )}
+                        </Formik>
+                        <Box className='boxOr' sx={{
                             display: 'flex',
                             justifyContent: 'center',
                         }}>
@@ -54,30 +120,36 @@ const LoginPage = () => {
                             <span className='right' />
                         </Box>
                         {/* <Button color='secondary' className='gitButton' variant="contained" type="submit" size='large'>Login with Github</Button> */}
-                        <Box sx={{ py: 2 }}>
+                        <Box sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            py: 2
+                        }}>
                             <Button
                                 className='gitButton'
                                 color="secondary"
-                                variant="contained"
                                 fullWidth
+                                size='large'
+                                variant="contained"
                                 type="submit"
-                                size="large"
+                                onClick={handleGithubLogin}
                             >
                                 Login with Github
                             </Button>
-                        </Box>
-                        <Box sx={{ py: 1 }}>
+
                             <Button
                                 className='googleButton'
                                 color="secondary"
-                                variant="contained"
                                 fullWidth
+                                size='large'
+                                variant="contained"
                                 type="submit"
-                                size="large"
+                                onClick={handleGoogleLogin}
                             >
                                 Login with Google
                             </Button>
                         </Box>
+                        <Typography variant='h7' className='register'>Don't Have An Account? Register <Link to='/register' className='link'>Here</Link></Typography>
                     </Box>
                 </Paper>
             </Box>
