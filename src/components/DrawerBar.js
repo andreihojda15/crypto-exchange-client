37884c0693122d2499c927f5462deee8741d26e9
andreihojda15 <andrei.hojda00@gmail.com>
2022-06-07 21:57:16 +0300

    [FE] MainPage: implemented drawerbar as a component

---
+++ src/components/DrawerBar.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import image1 from '../imgs/image1.png'
+import MovingIcon from '@mui/icons-material/Moving';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import './drawerbar.css'
+
+const DrawerBar = () => {
+    const logout = () => {
+        // do a logout
+    }
+
+    return (
+        <Drawer variant="permanent"
+            sx={{
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    overflow: 'hidden',
+                    position: 'relative',
+                    width: '350px',
+                    height: '600px'
+                },
+            }}
+            open
+        >
+            <Avatar
+                alt='user profile picture'
+                src={image1}
+                className='picture'
+                sx={{
+                    width: 100,
+                    height: 100,
+                }}
+            />
+            <Typography variant='h4' sx={{ position: 'relative', top: 70, left: 120 }}>John Doe</Typography>
+            <Typography
+                sx={{
+                    position: 'relative',
+                    top: 80,
+                    left: 140,
+                    fontSize: 12,
+                    color: 'rgba(0, 0, 0, 0.38)',
+                }}>Joined 10/06/2022</Typography>
+            <List className='list'>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <MovingIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Transaction History' />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <TimelineIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Currency ratio history' />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AccountBoxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Profile' />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Button
+                type='button'
+                variant='contained'
+                size='large'
+                className='logoutButton'
+                onClick={logout}
+            >
+                Log out
+            </Button>
+        </Drawer>
+    )
+}
+
+export default DrawerBar
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,40 +2,10 @@ import React from 'react'
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import './mainpage.css'
-import { Button, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import image1 from '../imgs/image1.png'
-import MovingIcon from '@mui/icons-material/Moving';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import TimelineIcon from '@mui/icons-material/Timeline';
-import {
-    DataGrid,
-    gridPageCountSelector,
-    gridPageSelector,
-    useGridApiContext,
-    useGridSelector,
-  } from '@mui/x-data-grid';
-  import Pagination from '@mui/material/Pagination';
-
-  function CustomPagination() {
-    const apiRef = useGridApiContext();
-    const page = useGridSelector(apiRef, gridPageSelector);
-    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
-  
-    return (
-      <Pagination
-        color="primary"
-        count={pageCount}
-        page={page + 1}
-        onChange={(event, value) => apiRef.current.setPage(value - 1)}
-      />
-    );
-  }
+import DrawerBar from '../components/DrawerBar'
 
 const MainPage = () => {
-    const logout = () => {
-        // do a logout
-    }
+    
 
     return (
         <>
@@ -49,72 +19,7 @@ const MainPage = () => {
                         height: 600,
                     }}
                 >
-                    <Drawer variant="permanent"
-                        sx={{
-                            flexShrink: 0,
-                            '& .MuiDrawer-paper': {
-                                overflow: 'hidden',
-                                position: 'relative',
-                                width: '350px',
-                                height: '600px'
-                            },
-                        }}
-                        open
-                    >
-                        <Avatar
-                            alt='user profile picture'
-                            src={image1}
-                            className='picture'
-                            sx={{
-                                width: 100,
-                                height: 100,
-                            }}
-                        />
-                        <Typography variant='h4' sx={{ position: 'relative', top: 70, left: 120 }}>John Doe</Typography>
-                        <Typography
-                            sx={{
-                                position: 'relative',
-                                top: 80,
-                                left: 140,
-                                fontSize: 12,
-                                color: 'rgba(0, 0, 0, 0.38)',
-                            }}>Joined 10/06/2022</Typography>
-                        <List className='list'>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <MovingIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary='Transaction History' />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TimelineIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary='Currency ratio history' />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <AccountBoxIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary='Profile' />
-                                </ListItemButton>
-                            </ListItem>
-                        </List>
-                        <Button
-                            type='button'
-                            variant='contained'
-                            size='large'
-                            className='logoutButton'
-                            onClick={logout}
-                        >
-                            Log out
-                        </Button>
-                    </Drawer>
+                    <DrawerBar />
                 </Paper>
             </Box>
         </>
