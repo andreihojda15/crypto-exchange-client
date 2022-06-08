fb9c748ba94ffc34c28299f877afbb37e364a06e
Sebi <stoicescusebastian@yahoo.com>
2022-06-08 14:40:32 +0300

    [FE] create register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box'
-import LoginPage from './pages/LoginPage'
-import Navbar from './components/Navbar'
+import LoginPage from './pages/Login/LoginPage'
+import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Register from 'pages/Register'
 import MainPage from 'pages/MainPage'
@@ -9,7 +9,7 @@ export default function App() {
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1}} style={{ height: '100%', width: '100%'}}>
         <Navbar />
         <Routes>
           <Route path='/login' element={<LoginPage />} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box
+          sx={{
+            position: 'absolute',
+            left: 100,
+          }}
+        >
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Crypto Exchange
+          </Typography>
+          <Typography variant="subtitle2" className="undertext">
+            by <b>Cognizant</b> Soft<b>vision</b>
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
