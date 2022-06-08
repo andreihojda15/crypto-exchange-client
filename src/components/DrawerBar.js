a9a33953c3b1b4498e68ae9f4c35636f53671699
andreihojda15 <andrei.hojda00@gmail.com>
2022-06-08 14:35:30 +0300

    [FE] drawer: implemented drawer with grid layout

--- src/components/DrawerBar.js
+++ src/components/DrawerBar.js
@@ -5,9 +5,9 @@ import image1 from '../imgs/image1.png'
 import MovingIcon from '@mui/icons-material/Moving';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import TimelineIcon from '@mui/icons-material/Timeline';
-import './drawerbar.css'
+import { Box } from '@mui/system';
 
-const DrawerBar = () => {
+export const DrawerBar = () => {
     const logout = () => {
         // do a logout
     }
@@ -15,71 +15,82 @@ const DrawerBar = () => {
     return (
         <Drawer variant="permanent"
             sx={{
-                flexShrink: 0,
                 '& .MuiDrawer-paper': {
-                    overflow: 'hidden',
                     position: 'relative',
-                    width: '350px',
-                    height: '600px'
                 },
             }}
             open
         >
-            <Avatar
-                alt='user profile picture'
-                src={image1}
-                className='picture'
-                sx={{
-                    width: 100,
-                    height: 100,
-                }}
-            />
-            <Typography variant='h4' sx={{ position: 'relative', top: 70, left: 120 }}>John Doe</Typography>
-            <Typography
-                sx={{
-                    position: 'relative',
-                    top: 80,
-                    left: 140,
-                    fontSize: 12,
-                    color: 'rgba(0, 0, 0, 0.38)',
-                }}>Joined 10/06/2022</Typography>
-            <List className='list'>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <MovingIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='Transaction History' />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <TimelineIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='Currency ratio history' />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AccountBoxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='Profile' />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-            <Button
-                type='button'
-                variant='contained'
-                size='large'
-                className='logoutButton'
-                onClick={logout}
-            >
-                Log out
-            </Button>
+            <Box sx={{
+                display: 'grid',
+                gridTemplateAreas: `'avatar'
+                                    'name'
+                                    'date'
+                                    'list'
+                                    'logout'`,
+                gridTemplateRows: '100px 50px 20px 300px auto',
+                justifyItems: 'center',
+            }}>
+                <Avatar
+                    alt='user profile picture'
+                    src={image1}
+                    sx={{
+                        width: 100,
+                        height: 100,
+                        gridArea: 'avatar',
+                    }}
+                />
+                <Typography variant='h4' sx={{
+                    gridArea: 'name',
+                }}>John Doe</Typography>
+                <Typography
+                    sx={{
+                        gridArea: 'date',
+                        fontSize: 12,
+                        color: 'rgba(0, 0, 0, 0.38)',
+                    }}>Joined 10/06/2022</Typography>
+                <List sx={{
+                    gridArea: 'list',
+                }}>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <MovingIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Transaction History' />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <TimelineIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Currency ratio history' />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <AccountBoxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Profile' />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                <Button
+                    type='button'
+                    variant='contained'
+                    size='large'
+                    onClick={logout}
+                    sx={{
+                        width: 220,
+                        marginY: 5,
+                        gridArea: 'logout',
+                    }}
+                >
+                    Log out
+                </Button>
+            </Box>
         </Drawer>
     )
 }
-
-export default DrawerBar
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import './mainpage.css'
-import DrawerBar from '../components/DrawerBar'
+import { DrawerBar } from '../components/DrawerBar'
 
 const MainPage = () => {
     
@@ -15,8 +15,7 @@ const MainPage = () => {
                 <Paper
                     elevation={8}
                     sx={{
-                        width: 1200,
-                        height: 600,
+                        
                     }}
                 >
                     <DrawerBar />
