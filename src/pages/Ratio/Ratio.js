864dc5d9d54f1ff7e4a9507393d2949121e3dd69
andreihojda15 <andrei.hojda00@gmail.com>
2022-06-10 13:43:12 +0300

    [FE] recharts: added chart on paper

--- src/pages/Ratio/Ratio.js
+++ src/pages/Ratio/Ratio.js
@@ -2,8 +2,25 @@ import { Paper } from '@mui/material'
 import { Box } from '@mui/system'
 import { DrawerBar } from 'components/Drawer/DrawerBar'
 import React from 'react'
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
+import './ratio.css'
 
 export const Ratio = () => {
+    const data = [
+        { name: 'Jan', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Feb', uv: 500, pv: 2400, amt: 2400 },
+        { name: 'Mar', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Apr', uv: 100, pv: 2400, amt: 2400 },
+        { name: 'May', uv: 500, pv: 2400, amt: 2400 },
+        { name: 'Jun', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Jul', uv: 600, pv: 2400, amt: 2400 },
+        { name: 'Aug', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Sep', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Oct', uv: 1000, pv: 2400, amt: 2400 },
+        { name: 'Nov', uv: 400, pv: 2400, amt: 2400 },
+        { name: 'Dec', uv: 400, pv: 2400, amt: 2400 },
+    ];
+
     return (
         <>
             <Box className='container' sx={{
@@ -17,13 +34,19 @@ export const Ratio = () => {
                         gridTemplateColumns: '1fr 4fr',
                         gridTemplateRows: '50px 700px',
                         gridTemplateAreas: `'drawer .'
-                                            'drawer .'
+                                            'drawer charts'
                                             'drawer .'`,
                         justifyContent: 'center',
                         width: '90%',
                     }}
                 >
                     <DrawerBar notHome={true} />
+                    <LineChart width={1000} height={500} data={data} className='charts'>
+                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                    </LineChart>
                 </Paper>
             </Box>
         </>
